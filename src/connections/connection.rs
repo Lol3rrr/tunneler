2f414d4acf2cb7e12abdf2f5527edd68fa98d556
lol3rrr <s.loler03@gmail.com>
2021-02-02 12:11:42 +0100

    Added a buffered forwarding thing

--- src/connections/connection.rs
+++ src/connections/connection.rs
@@ -48,13 +48,12 @@ impl Connection {
         self.stream.try_write(buf)
     }
 
-    pub async fn forward_to_connection(
+    /// This method reads the Data from connection and simply forwards it in the same chunks
+    async fn unbuffered_forward(
         &self,
         header: &MessageHeader,
         out: std::sync::Arc<Self>,
     ) -> std::io::Result<()> {
-        let buffer_response = true;
-
         let total_length = header.get_length() as usize;
         let mut left_to_read = total_length;
 
@@ -94,6 +93,76 @@ impl Connection {
 
         debug!("[{}] Done forwarding", header.get_id());
 
-        return Ok(());
+        Ok(())
+    }
+    /// This method waits until it has received the entire Data and then forwards it all in
+    /// one big message
+    async fn buffered_forward(
+        &self,
+        header: &MessageHeader,
+        out: std::sync::Arc<Self>,
+    ) -> std::io::Result<()> {
+        let total_length = header.get_length() as usize;
+        let mut message_buffer = Vec::with_capacity(total_length);
+
+        let mut offset = 0;
+        let mut left_to_read = total_length;
+
+        while left_to_read > 0 {
+            debug!(
+                "[{}] {} Bytes out of {} left to read",
+                header.get_id(),
+                left_to_read,
+                total_length
+            );
+
+            match self
+                .read(&mut message_buffer[offset..offset + left_to_read])
+                .await
+            {
+                Ok(0) => {
+                    error!("[{}] Read 0 Bytes", header.get_id());
+                    return Err(std::io::Error::from(std::io::ErrorKind::ConnectionReset));
+                }
+                Ok(n) => {
+                    offset += n;
+                    left_to_read -= n;
+                    debug!("[{}] Read {} Bytes", header.get_id(), n);
+                }
+                Err(ref e) if e.kind() == std::io::ErrorKind::WouldBlock => {
+                    continue;
+                }
+                Err(e) => {
+                    return Err(e);
+                }
+            };
+        }
+
+        debug!("[{}] Done buffering message", header.get_id());
+
+        match out.write(&message_buffer).await {
+            Ok(_) => {}
+            Err(e) => {
+                return Err(e);
+            }
+        };
+
+        debug!("[{}] Done forwarding", header.get_id());
+
+        Ok(())
+    }
+
+    pub async fn forward_to_connection(
+        &self,
+        header: &MessageHeader,
+        out: std::sync::Arc<Self>,
+    ) -> std::io::Result<()> {
+        let buffer_response = true;
+
+        if buffer_response {
+            self.buffered_forward(header, out).await
+        } else {
+            self.unbuffered_forward(header, out).await
+        }
     }
 }
